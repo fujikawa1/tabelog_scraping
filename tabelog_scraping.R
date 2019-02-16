699c31cfbf41d7c70a9d5ee48dde57af885ee23b
fujikawa1 <47475538+fujikawa1@users.noreply.github.com>
2019-02-16 17:20:25 +0900

    Update tabelog_scraping.R

--- tabelog_scraping.R
+++ tabelog_scraping.R
@@ -2,51 +2,51 @@
 library(rvest)
 
 
-###�P�A�H�׃��O�̃T�C�g�������擾����֐�tabelog_scraper_1p##
+###１、食べログのサイト内情報を取得する関数tabelog_scraper_1p###
 
 tabelog_scraper_1p <- function(url) {
   html <- read_html(url)
-  ##�f�[�^�̎擾
-  #���X�ܐ��̎擾
+  ##データの取得
+  #総店舗数の取得
   nShop_raw <- html_nodes(html, ".page-count") %>%
     html_text(.)
   
-  #�X�ܖ��̎擾
+  #店舗名の取得
   shopName <- html_nodes(html, ".cpy-rst-name") %>% 
     html_text(.) 
   
-  #�H�׃��O�X�R�A�̎擾
+  #食べログスコアの取得
   score <- html_nodes(html, ".list-rst__rating-val") %>%  
     html_text(.) 
   
-  #���r���[���̎擾
+  #レビュー数の取得
   nReview <- html_nodes(html, ".list-rst__rvw-count-target") %>% 
     html_text(.) 
   
-  #���̑��֘A���̎擾
+  #その他関連情報の取得
   info <- html_nodes(html, ".cpy-area-genre") %>% 
     html_text(.)
   
   
-  ##�f�[�^�̐��`
-  #�f�[�^�̒�������
+  ##データの整形
+  #データの長さ調整
   n <- 20
   length(shopName) = n
   length(score) = n
   length(nReview) = n
   length(info) = n
   
-  #nShop_raw���當����r��
+  #nShop_rawから文字を排除
   nShop_raw <- substr(nShop_raw, 22, 28)
   length(nShop_raw) = n
   
-  #nReview���當����r��
-  nReview <- gsub("��","",nReview)
+  #nReviewから文字を排除
+  nReview <- gsub("件","",nReview)
   
-  #�擾�����f�[�^���f�[�^�t���[���ɂ܂Ƃ߂�
+  #取得したデータをデータフレームにまとめる
   tabelog_data[[pagenum]] <<- na.omit(data.frame(shopName,score,nReview,info))
   
-  #�T�[�o�[�ւ̕��ׂ��y�����邽��10�b�x�~
+  #サーバーへの負荷を軽減するため10秒休止
   Sys.sleep(10)
 } 
 
@@ -55,14 +55,14 @@ tabelog_scraper_1p <- function(url) {
 
 
 
-###2�A�H�׃��O�T�C�g���̏��𕡐��y�[�W�ɓn���Ď擾����֐�tabelog_scraper###
+###2、食べログサイト内の情報を複数ページに渡って取得する関数tabelog_scraper###
 
 tabelog_scraper <- function(url) {
-  #�ϐ���url����
+  #変数にurlを代入
   
   
   
-  #�f�[�^�̎擾
+  #データの取得
   html <- read_html(url)
   nShop_raw <- html_nodes(html, ".c-page-count") %>%
     html_text(.)
@@ -79,52 +79,52 @@ tabelog_scraper <- function(url) {
   info <- html_nodes(html, ".cpy-area-genre") %>% 
     html_text(.)
   
-  #�f�[�^�̐��`
+  #データの整形
   n <- 20
   length(shopName) = n
   length(score) = n
   length(nReview) = n
   length(info) = n
   
-  #nShop_raw���當����r��
+  #nShop_rawから文字を排除
   nShop_raw <- substr(nShop_raw, 22, 28)
   pagesum <<- ceiling((as.numeric(substr(nShop_raw, 3, 5)))/20)
   length(nShop_raw) = n
   
-  #nReview���當����r��
-  nReview <- gsub("��","",nReview)
+  #nReviewから文字を排除
+  nReview <- gsub("件","",nReview)
   
-  #�f�[�^�t���[���Ƀf�[�^���܂Ƃ߂�
+  #データフレームにデータをまとめる
   tabelog_data[[1]] <<- na.omit(data.frame(shopName,score,nReview,info))
   
-  #�i�s�󋵊m�F�p�̏o��
-  print("1�y�[�W��")
+  #進行状況確認用の出力
+  print("1ページ目")
   
-  #�T�[�o�[�ւ̕��ׂ��y�����邽��10�b�x�~
+  #サーバーへの負荷を軽減するため10秒休止
   Sys.sleep(10)
   
-  #tabelog_scraper_1p���y�[�W���̕������J��Ԃ�
+  #tabelog_scraper_1pをページ数の分だけ繰り返す
   while(pagenum < pagesum) {
     url <- paste("https://tabelog.com/osaka/A2701/A270404/R368/rstLst/",as.integer(pagenum),"/?Srt=D&SrtT=rt&sort_mode=1&svd=20181206&svt=1900&svps=2",sep = "")
     tabelog_scraper_1p(url)
-    print(paste(pagenum,"�y�[�W��",sep=""))
+    print(paste(pagenum,"ページ目",sep=""))
     pagenum <<- pagenum + 1
   }
   return(tabelog_data)
 } 
 
 
-###�R�A�g�p����l�̓���
-#�擾�J�n�y�[�W�̎w��
+###３、使用する値の入力###
+#取得開始ページの指定
 pagenum <<- 2
 
-#�擾�y�[�W��url�̎w��
+#取得ページのurlの指定
 url1 <<- "https://tabelog.com/osaka/A2701/A270404/R368/rstLst/?Srt=D&SrtT=rt&sort_mode=1&svd=20181206&svt=1900&svps=2"
 
-#�f�[�^�ۊǗp�̃��X�g�̍쐬
+#データ保管用のリストの作成
 tabelog_data <- list()
 
 
-###�S�A�f�[�^�̏o��
+###４、データの出力###
 data <- tabelog_scraper(url)
 data
